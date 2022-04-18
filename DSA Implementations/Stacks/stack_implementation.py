6a7a1f3e93b262b358d85855c29d2604090f1418
oumburs9 <oumburs9mry@gmail.com>
2022-04-19 00:26:08 +0300

    stack implementation (push,pop, peek , size and is_empty methods

---
+++ DSA Implementations/Stacks/stack_implementation.py
@@ -0,0 +1,32 @@
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self,data):
+        self.stack.append(data)
+
+    def pop(self):
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size_stack(self):
+        return len(self.stack)
+        
+    def is_empty(self):
+        return self.stack == []
+stack = Stack()
+stack.push(1)
+stack.push(11)
+stack.push(111)
+stack.push(1111)
+print("size: %d" % stack.size_stack())
+print("pop.: %d" % stack.pop())
+print("size: %d" % stack.size_stack())
+print("peek: %d" % stack.peek())
+print("size: %d" % stack.size_stack())
+
