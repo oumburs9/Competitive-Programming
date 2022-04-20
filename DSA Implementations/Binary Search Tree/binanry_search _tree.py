0801b23657875f94356066434470bdac15d240cb
oumburs9 <oumburs9mry@gmail.com>
2022-04-20 17:36:47 +0300

    insertion and traversal of binary search tree

--- DSA Implementations/Binary Search Tree/binanry_search _tree.py
+++ DSA Implementations/Binary Search Tree/binanry_search _tree.py
@@ -1,8 +1,5 @@
-from multiprocessing import parent_process
-
-
 class Node:
-    def __init__(self,data):
+    def __init__(self,data,parent):
         self.data = data
         self.leftChild = None
         self.rightChild = None
@@ -11,21 +8,42 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
     def insert(self, data):
-        if self.root:
-            self.root = Node(data)
+        if  self.root is None:
+            self.root = Node(data,None)
         else:
-            self.root = self.insert_node(data, self.root)
-    def insert(self,data,node):
+            self.insert_node(data, self.root)
+
+    def insert_node(self,data,node):
         if data < node.data:
-            if self.leftChild:
+            if node.leftChild:
                 self.insert_node(data,node.leftChild)
             else:
                 node.leftChild = Node(data,node)
         else:
-            if self.rightChild:
+            if node.rightChild:
                 self.insert_node(data,node.rightChild)
             else:
                 node.rightChild = Node(data,node)
-                
+    def traverse(self):
+        if self.root is not None:
+            self.traverse_in_order(self.root)
+    def traverse_in_order(self,node):
+
+        if node.leftChild:
+            self.traverse_in_order(node.leftChild)
+        print("%s" % node.data)
+
+        if node.rightChild:
+            self.traverse_in_order(node.rightChild)
+
+bst = BinarySearchTree()
+bst.insert(1)
+bst.insert(7)
+bst.insert(4)
+bst.insert(-3)
+
+bst.traverse()
+
 
