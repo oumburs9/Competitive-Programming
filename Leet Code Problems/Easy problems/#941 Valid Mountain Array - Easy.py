598cf113c2cf0e1405a8a774ac9f271d9b574bdf
oumburs9 <oumburs9mry@gmail.com>
2022-11-28 20:02:23 +0300

    valid mountain arry modification

--- Leet Code Problems/Easy problems/#941 Valid Mountain Array - Easy.py
+++ Leet Code Problems/Easy problems/#941 Valid Mountain Array - Easy.py
@@ -1,12 +1,15 @@
 def validMountain(arr):
-    index = 1
-    while(index <len(arr) and arr[index]>arr[index-1]):
-        index += 1
-    if(index ==1 or index ==len(arr)):
+    if (len(arr) < 3):
         return False
-    while(index<len(arr)and arr[index]<arr[index-1]):
-        index +=1
-    return index ==1 or index ==len(arr)
+    index = 1
+    while (index < len(arr) and arr[index] > arr[index-1]):
+        index += 1
+    if (index == 1 or index == len(arr)):
+        return False
+    while (index < len(arr) and arr[index] < arr[index-1]):
+        index += 1
+    return index == 1 or index == len(arr)
 
-arr = [1,2,3,8,6,9]
+
+arr = [1, 2, 3, 8, 6, 9]
 print(validMountain(arr=arr))
