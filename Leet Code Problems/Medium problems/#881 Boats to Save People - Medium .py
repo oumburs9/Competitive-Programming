990ab39304b36041d598cca40b58e05c28f7b57b
oumburs9 <oumburs9mry@gmail.com>
2022-11-28 19:18:16 +0300

    min num of boat to save people

---
+++ Leet Code Problems/Medium problems/#881 Boats to Save People - Medium .py
@@ -0,0 +1,21 @@
+def numRescueBoats(people, limit):
+    people.sort()
+
+    left = 0
+    right = len(people)-1
+
+    boats_number = 0
+
+    while(left<=right):
+        if(left==right):
+            boats_number+=1
+            break
+        if(people[left]+people[right]<=limit):
+            left+=1
+
+        right-=1
+        boats_number+=1
+    return boats_number
+people = [4,6,11,19,9]
+limit = 15
+print(numRescueBoats(people,limit))
