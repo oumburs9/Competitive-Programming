c61fb02a4882bdaf10953e331518f3c7aeaade67
burs <oum.burs9mry@gmail.com>
2023-01-18 23:31:59 +0300

    prefixSum.....

---
+++ Leet Code Problems/Medium problems/#560 Subarray Sum Equals K -medium.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        currentSum = 0
+        
+        prefixSum = {0:1}
+        
+        
+        for i in nums:
+            currentSum +=i
+            diff = currentSum - k
+            
+            ans += prefixSum.get(diff,0)
+            prefixSum[currentSum] = 1 + prefixSum.get(currentSum,0)
+        return ans
+        
+        
+        
