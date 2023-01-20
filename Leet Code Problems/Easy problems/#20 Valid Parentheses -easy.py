4373652e3f4d3d722a7d17924f5351452cca880e
burs <oum.burs9mry@gmail.com>
2023-01-20 22:41:18 +0300

    min stack

--- Leet Code Problems/Easy problems/#20 Valid Parentheses -easy.py
+++ Leet Code Problems/Easy problems/#20 Valid Parentheses -easy.py
@@ -16,6 +16,22 @@ class Solution:
                 return False
         return len(stack) == 0
 
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         dic = {")": "(", "]": "[", "}": "{"}
+#         stack = []
+
+#         for c in s:
+#             if c not in dic:
+#                 stack.append(c)
+#                 continue
+#             if not stack or stack[-1] != dic[c]:
+#                 return False
+#             stack.pop()
+
+#         return not stack
+
+
                 
                 
                 
