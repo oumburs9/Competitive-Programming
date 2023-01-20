0b6588d7d502ed13bfa9ee6cbf97f49544a2fd7d
burs <oum.burs9mry@gmail.com>
2023-01-20 21:32:21 +0300

    valid parentheses stack:

---
+++ Leet Code Problems/Easy problems/#20 Valid Parentheses -easy.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        dic ={
+            "{":"}",
+            "[":"]",
+            "(":")"
+        }
+
+        
+        stack = []
+        for i in s:
+            if i in dic:
+                stack.append(i)
+            elif len(stack)==0 or dic[stack.pop()] != i:
+                return False
+        return len(stack) == 0
+
+                
+                
+                
+        
+        
+        
+        
