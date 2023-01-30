a7149e7b7bc5ca522f9f4a5977cc6df44f2de358
burs <oum.burs9mry@gmail.com>
2023-01-30 22:33:38 +0300

    palindrome linked list

---
+++ Leet Code Problems/Easy problems/#234 Palindrome Linked List -easy.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+    
+    
+        stack = []
+        
+        fast = head
+        slow = head
+        
+        # find the middle (slow) and push to stack
+        while fast and fast.next:
+            stack.append(slow.val)
+            
+            fast = fast.next.next
+            slow = slow.next
+        if fast != None:
+            slow = slow.next
+        # print(stack)
+            # stack.append(slow.next.val)
+
+        
+        while slow and stack:
+            if stack.pop() == slow.val:
+                slow = slow.next
+            else:
+                return False
+        return True
+            
+            
+            
+        
+        
+        
