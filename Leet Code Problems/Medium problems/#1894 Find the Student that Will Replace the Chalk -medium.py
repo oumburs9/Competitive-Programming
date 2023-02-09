7ffd91ce6344b59f66da600c42796423667adbe2
burs <oum.burs9mry@gmail.com>
2023-02-09 23:49:11 +0300

    chalk replacer ...

--- Leet Code Problems/Medium problems/#1894 Find the Student that Will Replace the Chalk -medium.py
+++ Leet Code Problems/Medium problems/#1894 Find the Student that Will Replace the Chalk -medium.py
@@ -3,8 +3,8 @@ from typing import List
 
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        sum_ = sum(chalk)
-        max_ = k % sum_
+        pSum_ = sum(chalk)
+        max_ = k % pSum_
        
         for i in range(len(chalk)):
             if max_-chalk[i] >= 0:
