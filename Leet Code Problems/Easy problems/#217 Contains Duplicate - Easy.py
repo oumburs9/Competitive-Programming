1a7d0ffc341bdfadd97b3ab97aa0c84d2b5b237c
burs <oum.burs9mry@gmail.com>
2023-03-03 09:37:10 +0300

    duplicate using sets

--- Leet Code Problems/Easy problems/#217 Contains Duplicate - Easy.py
+++ Leet Code Problems/Easy problems/#217 Contains Duplicate - Easy.py
@@ -1,3 +1,6 @@
+from typing import List
+
+#approach 1 using map
 def dublicateNum(nums):
     m = {}
     for i in range(len(nums)):
@@ -8,3 +11,15 @@ def dublicateNum(nums):
 
 res =dublicateNum([1,2,3,1])
 print(res)
+
+# approach 2 using sets
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        window = set()
+        
+        for e in range(len(nums)):
+            if nums[e] in window:
+                return True
+            window.add(nums[e])
+        return False
