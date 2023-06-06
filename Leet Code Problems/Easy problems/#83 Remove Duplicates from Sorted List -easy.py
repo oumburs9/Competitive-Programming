fbc8aed100ec5a068bdb602520c8dc99e9272c32
Oum Ja <oum.burs9mry@gmail.com>
2023-06-06 22:22:54 +0300

    remove duplicates from sorted list

--- Leet Code Problems/Easy problems/#83 Remove Duplicates from Sorted List -easy.py
+++ Leet Code Problems/Easy problems/#83 Remove Duplicates from Sorted List -easy.py
@@ -2,10 +2,10 @@
 from typing import Optional
 
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur = head
