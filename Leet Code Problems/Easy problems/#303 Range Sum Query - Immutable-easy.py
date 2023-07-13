6cc8128c19a90f67f093e89eafd82cda7af9d65e
Oum Ja <oum.burs9mry@gmail.com>
2023-07-13 21:31:51 +0300

    another approach for rangesum

--- Leet Code Problems/Easy problems/#303 Range Sum Query - Immutable-easy.py
+++ Leet Code Problems/Easy problems/#303 Range Sum Query - Immutable-easy.py
@@ -1,22 +1,34 @@
 from typing import List
 
 
-class NumArray:
+# class NumArray:
 
+#     def __init__(self, nums: List[int]):
+#         self.prefix = []
+#         sum_ti = 0
+        
+#         for val in nums:
+#             sum_ti +=val
+#             self.prefix.append(sum_ti)
+        
+
+#     def sumRange(self, left: int, right: int) -> int:
+#         if left ==0:
+#             return self.prefix[right]
+#         else:
+#             return self.prefix[right] - self.prefix[left-1]
+
+
+class NumArray:
     def __init__(self, nums: List[int]):
-        self.prefix = []
-        sum_ti = 0
-        
-        for val in nums:
-            sum_ti +=val
-            self.prefix.append(sum_ti)
-        
+        self.prefix_sum = [0]
+        for num in nums:
+            self.prefix_sum.append(self.prefix_sum[-1] + num)
 
     def sumRange(self, left: int, right: int) -> int:
-        if left ==0:
-            return self.prefix[right]
-        else:
-            return self.prefix[right] - self.prefix[left-1]
+        leftSum = self.prefix_sum[left]
+        rightSum = self.prefix_sum[right+1]
+        return rightSum - leftSum
         
 
 
