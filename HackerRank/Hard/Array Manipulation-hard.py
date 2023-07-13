e11b00f6fb575266b60a46b0d3924f92a947d14d
Oum Ja <oum.burs9mry@gmail.com>
2023-07-14 01:13:31 +0300

    array manipulation hackerRank

---
+++ HackerRank/Hard/Array Manipulation-hard.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    manipulatedArr = [0] * (n + 1)
+    maxVal = -math.inf
+    prefixSum = 0
+
+    for i in range(len(queries)):
+        a = queries[i][0]
+        b = queries[i][1]
+        k = queries[i][2]
+
+        manipulatedArr[a - 1] += k
+        manipulatedArr[b] -= k
+
+    for i in range(n):
+        prefixSum += manipulatedArr[i]
+        maxVal = max(maxVal, prefixSum)
+
+    return maxVal
+            
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
