7c2255e98bedd34bea085e8627ab2f632236629e
Oum Ja <oum.burs9mry@gmail.com>
2023-07-14 20:31:05 +0300

    product array except self

--- Leet Code Problems/Medium problems/#238 Product of Array Except Self -medium.py
+++ Leet Code Problems/Medium problems/#238 Product of Array Except Self -medium.py
@@ -1,19 +1,49 @@
-from typing import List
+from ast import List
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        n = len(nums)
+        result = [1] * n
 
+        # Calculate prefix product
         prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
+        for i in range(n):
+            # Store the current prefix product in the result array
+            result[i] = prefix
+            # Update the prefix product for the next element
             prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+
+        # Calculate suffix product and construct result array
+        suffix_product = 1
+        for i in range(n-1, -1, -1):
+            # Multiply the current suffix product with the corresponding element in the result array
+            result[i] *= suffix_product
+            # Update the suffix product for the next element
+            suffix_product *= nums[i]
+
+        return result
+
+            
+            
+        
+
+# from typing import List
+
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         res = [1] * (len(nums))
+
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix *= nums[i]
+#         postfix = 1
+#         for i in range(len(nums) - 1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+#         return res
             
             
             
