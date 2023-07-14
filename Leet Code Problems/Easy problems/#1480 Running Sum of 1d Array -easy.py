bc7958466b3149fb42646ddde5bc2afdb2e3c26f
Oum Ja <oum.burs9mry@gmail.com>
2023-07-14 20:26:22 +0300

    runnig sum 1d array

--- Leet Code Problems/Easy problems/#1480 Running Sum of 1d Array -easy.py
+++ Leet Code Problems/Easy problems/#1480 Running Sum of 1d Array -easy.py
@@ -1,13 +1,27 @@
-from typing import List
+from ast import List
 
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        prefixSum = []
-        total = 0
+        runningSum = []
+        acc = 0
         
         for i in range(len(nums)):
-            total +=nums[i]
-            prefixSum.append(total)
-        return prefixSum
+            acc += nums[i]
+            runningSum.append(acc)
+        return runningSum
+
+
+# from typing import List
+
+
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+#         prefixSum = []
+#         total = 0
+        
+#         for i in range(len(nums)):
+#             total +=nums[i]
+#             prefixSum.append(total)
+#         return prefixSum
             
