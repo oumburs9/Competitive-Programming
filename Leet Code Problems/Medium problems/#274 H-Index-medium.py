fb17f28a72d939b45c6392cbb755022d3f3aa92a
Oum Ja <oum.burs9mry@gmail.com>
2023-07-21 19:45:23 +0300

    H-index

---
+++ Leet Code Problems/Medium problems/#274 H-Index-medium.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        left, right = 0, n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            h = n - mid
+            
+            if citations[mid] >= h:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return n - left      
