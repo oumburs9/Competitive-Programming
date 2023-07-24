298cb3359ebf8e256882374408ff916dd4ca9f59
Oum Ja <oum.burs9mry@gmail.com>
2023-07-24 12:12:54 +0300

    peak index in a mountain array

---
+++ Leet Code Problems/Medium problems/#852. Peak Index in a Mountain Array-medium.py
@@ -0,0 +1,16 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+
+        while l < r:
+            # for int overflow
+            mid = l + (r - l) // 2
+
+            if arr[mid] > arr[mid + 1]:
+                
+                r = mid
+            else:
+               
+                l = mid + 1
+
+        return l
