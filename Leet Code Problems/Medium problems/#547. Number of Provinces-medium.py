972de94888c81e752c1d6a1790c6582f95cf24db
Oum Ja <oum.burs9mry@gmail.com>
2023-09-23 20:47:41 +0300

    number of provices

---
+++ Leet Code Problems/Medium problems/#547. Number of Provinces-medium.py
@@ -0,0 +1,19 @@
+class Solution:
+    
+    def dfs(self, node, isConnected, visit):
+        visit[node] = True
+        for i in range(len(isConnected)):
+            if isConnected[node][i] == 1 and not visit[i]:
+                self.dfs(i, isConnected, visit)
+
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        numberOfComponents = 0
+        visit = [False] * n
+
+        for i in range(n):
+            if not visit[i]:
+                numberOfComponents += 1
+                self.dfs(i, isConnected, visit)
+
+        return numberOfComponents
