3745a2b95035e03c492cfb99cf2721b55b09c158
Oum Ja <oum.burs9mry@gmail.com>
2023-10-21 09:05:50 +0300

    water containing

--- Leet Code Problems/Medium problems/#11 Container With Most Water - Medium.py
+++ Leet Code Problems/Medium problems/#11 Container With Most Water - Medium.py
@@ -1,17 +1,34 @@
-def containerWithMostWat(arr):
-     l=0
-     r= len(arr)-1
-     maxArea = 0
+# def containerWithMostWat(arr):
+#      l=0
+#      r= len(arr)-1
+#      maxArea = 0
 
-     while(l<r):
-        maxArea = max(maxArea,min(arr[l],arr[r])*(r-l))
-        if (arr[l]<arr[r]):
-            l +=1
-        else:
-            r-=1
-     return maxArea
+#      while(l<r):
+#         maxArea = max(maxArea,min(arr[l],arr[r])*(r-l))
+#         if (arr[l]<arr[r]):
+#             l +=1
+#         else:
+#             r-=1
+#      return maxArea
 
 
-arr = [1,8,6,2,5,4,8,3,7]
-res =containerWithMostWat(arr)
-print(res)
+# arr = [1,8,6,2,5,4,8,3,7]
+# res =containerWithMostWat(arr)
+# print(res)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l,r = 0,len(height)-1
+        res = 0
+        
+        while l < r:
+            area = (r-l) *min(height[l],height[r])
+            
+            res = max(area,res)
+            if height[l] > height[r]:
+                r -=1
+            else:
+                l +=1
+        return res
+                
+        
