1982e1e1fce8ef17017829a08d5daa7bee1ad63b
Oum Ja <oum.burs9mry@gmail.com>
2023-10-21 13:55:13 +0300

    rotate array

--- Leet Code Problems/Medium problems/#189 Rotate Array-medium.py
+++ Leet Code Problems/Medium problems/#189 Rotate Array-medium.py
@@ -1,29 +1,23 @@
-# /**
-#  * @param {number[]} nums
-#  * @param {number} k
-#  * @return {void} Do not return anything, modify nums in-place instead.
-#  */
-# var rotate = function(nums, k) {
-    
-  
-#   k = k % nums.length;
-    
-#     reverse(nums, 0, nums.length - 1);
-#     reverse(nums, 0, k - 1);
-#     reverse(nums, k, nums.length - 1);
-    
-# };
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
-# let reverse = function(nums, start, end) {
-#     while (start < end) {
-#         let temp = nums[start];
-#         nums[start] = nums[end];
-#         nums[end] = temp;
-#         start++;
-#         end--;
-#     } 
-    
-# };
+        n = len(nums)
+        k = k % n
+        if k == 0:
+            return
+
+        rotated = [0] * n
+        for i in range(n):
+            rotated[(i + k) % n] = nums[i]
+
+        for i in range(n):
+            nums[i] = rotated[i]
+
+
+        
 
 
 def rotate(nums,k):
