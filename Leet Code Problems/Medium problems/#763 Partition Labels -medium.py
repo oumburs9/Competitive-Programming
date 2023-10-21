b8b9d90d02938a9c5126256c5607b001f7de715a
Oum Ja <oum.burs9mry@gmail.com>
2023-10-21 11:00:44 +0300

    partition label

--- Leet Code Problems/Medium problems/#763 Partition Labels -medium.py
+++ Leet Code Problems/Medium problems/#763 Partition Labels -medium.py
@@ -28,7 +28,26 @@ class Solution:
           
 
                 
-                
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndx = {}
+        res = []
+
+
+        for i,c in enumerate(s):
+            lastIndx[c] = i
+
+        start,end = 0,0
+
+        for i,c in enumerate(s):
+            end = max(end,lastIndx[c])
+
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
+        return res
+
+                   
             
         
         
