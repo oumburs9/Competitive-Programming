e6d02d656a2a2567a86bed145514c7726827e1c0
Oum Ja <oum.burs9mry@gmail.com>
2023-10-22 10:06:13 +0300

    two sum problem using dictionary

--- Leet Code Problems/Easy problems/#1 Two Sum - Easy.py
+++ Leet Code Problems/Easy problems/#1 Two Sum - Easy.py
@@ -1,10 +1,21 @@
-def twoSum(nums,target):
-    for i in range(len(nums)-1):
-        if(nums[i]+nums[i+1]==target):
-            return [i,i+1]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            # finding goal
+            complement = target - nums[i]
+            
+            # checking the if the goal was already in d
+            if complement in dic:
+                return [dic[complement],i]
+            # keeping the current val in dictionary 
+            dic[nums[i]] = i
+        return []
+     
 
-res = twoSum([2,7,11,15],9)
-print(res)
+
+
+        
 
 def twoSum2(nums,target):
     m = {}
