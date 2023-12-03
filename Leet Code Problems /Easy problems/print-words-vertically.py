09f8a58ee62f04ac3a0dad1006cac45d6aee3ae6
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2023-12-03 19:16:41 +0300

    Added solution for Print Words Vertically

--- Leet Code Problems /Easy problems/print-words-vertically.py
+++ Leet Code Problems /Easy problems/print-words-vertically.py
@@ -9,19 +9,12 @@ class Solution:
         for i in range(len(_list)):
             for j in range(length):
                 if j >= len(_list[i]):
-                    if res[j]:
-                        res[j] += ' '
-                    else:
-                        
-                        res[j] = " " + res[j]
-                 
+                    res[j] += ' '
+
                 else:
                     res[j] += _list[i][j]
                     
-                
-                    
-              
-        
+            
         return [re.rstrip() for re in res]
                            
             
