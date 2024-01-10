c4df5b49496b0432ca777945e4e102a6969c5a97
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-01-10 18:27:48 +0300

    Added solution for Maximize the Confusion of an Exam

---
+++ Leet Code Problems/dsa /week_one/maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        max_consecutive = 0
+        left = 0
+        max_count = 0  # Maximum count of 'T's or 'F's in the window
+        counts = {'T': 0, 'F': 0}
+
+        for right in range(len(answerKey)):
+            counts[answerKey[right]] += 1
+            max_count = max(max_count, counts['T'], counts['F'])
+
+            # Check if the window can be expanded
+            while (right - left + 1 - max_count) > k:
+                counts[answerKey[left]] -= 1
+                left += 1
+
+            max_consecutive = max(max_consecutive, right - left + 1)
+
+        return max_consecutive
+        
