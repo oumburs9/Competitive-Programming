27477f21824fff8275b114493ac93c96b9b09ae0
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-01-13 22:13:22 +0300

    Add solution for Fruit Into Baskets

---
+++ Leet Code Problems /dsa/leetcode/fruit-into-baskets.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        maxLen = 0
+        currLen = 0
+        l = 0
+        unique = 0
+        dic = Counter()
+        n =len(fruits)
+        
+        
+        for r in range(n):
+            dic[fruits[r]] +=1
+            
+            if dic[fruits[r]] == 1:
+                unique +=1
+                
+                
+            while unique > 2:
+                dic[fruits[l]]-=1
+                
+                if dic[fruits[l]] ==0:
+                    unique -= 1
+                l +=1
+            currLen = r -l +1
+            maxLen = max(maxLen,currLen)
+        return maxLen
+            
+            
+                    
+                    
+                 
+                
+        
