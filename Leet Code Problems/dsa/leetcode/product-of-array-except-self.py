be2cbf3527f05f54227c12222249eeb44ef36051
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-08 11:28:17 +0300

    Add solution for Product of Array Except Self

--- Leet Code Problems/dsa/leetcode/product-of-array-except-self.py
+++ Leet Code Problems/dsa/leetcode/product-of-array-except-self.py
@@ -1,26 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        result = [1] * n
+        result = [1]*n
 
-        # Calculate prefix product
+        # cal prefix
         prefix = 1
         for i in range(n):
-            # Store the current prefix product in the result array
             result[i] = prefix
-            # Update the prefix product for the next element
             prefix *= nums[i]
+        
 
-        # Calculate suffix product and construct result array
-        suffix_product = 1
-        for i in range(n-1, -1, -1):
-            # Multiply the current suffix product with the corresponding element in the result array
-            result[i] *= suffix_product
-            # Update the suffix product for the next element
-            suffix_product *= nums[i]
-
+        # cal suffix  and update res
+        suffix = 1
+        for i in range(n-1,-1,-1):
+            result[i] *= suffix
+            suffix *= nums[i]
+            
         return result
+        
 
-            
-            
         
