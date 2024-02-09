7786b6d8471770b62a76155887e0308dd122bc7d
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-09 14:06:53 +0300

    Add solution for Linked List Cycle

---
+++ Leet Code Problems/dsa/leetcode/linked-list-cycle.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        hare = head
+        turtle = head
+        while hare and turtle and hare.next:
+            hare = hare.next.next
+            turtle = turtle.next
+            if turtle == hare:
+                return True
+        return False
