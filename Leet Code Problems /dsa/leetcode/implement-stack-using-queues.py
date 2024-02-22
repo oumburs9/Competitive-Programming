247ef871f814a26a8db45d9792fb9f5acd51ca14
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-22 11:48:53 +0300

    Add solution for Implement Stack using Queues

---
+++ Leet Code Problems /dsa/leetcode/implement-stack-using-queues.py
@@ -0,0 +1,43 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue_1 = []
+        self.queue_2 = []
+        
+
+    def push(self, x: int) -> None:
+        self.queue_1.append(x)
+
+    def pop(self) -> int:
+        if self.queue_2 and not self.queue_1:
+            while self.queue_2:
+                self.queue_1.append(self.queue_2.pop(0))
+
+        while len(self.queue_1) > 1:
+            self.queue_2.append(self.queue_1.pop(0))
+
+        return self.queue_1.pop(0)  
+
+    def top(self) -> int:
+        if self.queue_2 and not self.queue_1:
+            while self.queue_2:
+                self.queue_1.append(self.queue_2.pop(0))
+
+        while len(self.queue_1) > 1:
+            self.queue_2.append(self.queue_1.pop(0))
+
+        if self.queue_1:
+            return self.queue_1[-1]  
+        
+        
+    def empty(self) -> bool:
+        return len(self.queue_1) == 0 and len(self.queue_2) == 0
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
