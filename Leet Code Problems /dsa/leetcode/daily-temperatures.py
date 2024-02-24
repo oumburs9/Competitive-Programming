fcc76aea871a2a33cde80b0c2007cbaa777feffc
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-24 16:21:03 +0300

    Add solution for Daily Temperatures

--- Leet Code Problems /dsa/leetcode/daily-temperatures.py
+++ Leet Code Problems /dsa/leetcode/daily-temperatures.py
@@ -1,16 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        result = [0] * n  # Initialize arr with zeros
-        stack = []  # Stack to store indices of temperatures in decreasing order of values
+        result = [0] * n
+        stack = [] 
 
         for i in range(n):
-            # If the current temperature is higher than the temperature at the top of the stack,
-            # update the result for the index at the top of the stack
             while stack and temperatures[i] > temperatures[stack[-1]]:
                 index = stack.pop()
                 result[index] = i - index
 
-            stack.append(i)  # Push the current index
+            stack.append(i)  
 
         return result
