c1fcf711e9b4f045175733276a6e022d2b013908
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-26 21:42:48 +0300

    Add solution for Combinations

---
+++ Leet Code Problems/dsa/leetcode/combinations.py
@@ -0,0 +1,43 @@
+class Solution:
+     def combine(self, n: int, k: int) -> List[List[int]]:   
+        ans = []
+        path = []
+        
+        def backtrack(num):
+
+            if len(path) == k:
+                ans.append(path.copy())
+                return
+            
+
+            for i in range(num + 1,n+1):
+
+                path.append(i)
+
+                backtrack(i)
+              
+                path.pop()
+                
+        backtrack(0)
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
