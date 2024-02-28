077ba0064a4eeb728c650b2b1e2b48e764178d93
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-28 16:32:19 +0300

    Add solution for Maximum Difference Between Node and Ancestor

--- Leet Code Problems/dsa/leetcode/maximum-difference-between-node-and-ancestor.py
+++ Leet Code Problems/dsa/leetcode/maximum-difference-between-node-and-ancestor.py
@@ -6,19 +6,17 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        maxDif = 0
         def dfs(node, currMax, currMin):
-            nonlocal maxDif
             if not node:
-                return 
+                return currMax - currMin
             
             currMax = max(currMax, node.val)
             currMin = min(currMin, node.val)
-            maxDif = max(maxDif,currMax - currMin)
 
-            dfs(node.left, currMax, currMin)
-            dfs(node.right, currMax, currMin)
+            left = dfs(node.left, currMax, currMin)
+            right = dfs(node.right, currMax, currMin)
 
-        dfs(root, root.val, root.val)
-        return maxDif
+            return max(left, right)
+
+        return dfs(root, root.val, root.val)
         
