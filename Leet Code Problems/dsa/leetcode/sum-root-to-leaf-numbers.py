ec4e552482026ceb12cefbf426b56e4b68906313
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-02-29 18:02:45 +0300

    Add solution for Sum Root to Leaf Numbers

--- Leet Code Problems/dsa/leetcode/sum-root-to-leaf-numbers.py
+++ Leet Code Problems/dsa/leetcode/sum-root-to-leaf-numbers.py
@@ -7,20 +7,16 @@
 
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        return self.helper(root, 0)  # Start the computation by calling the helper function
-
+        return self.helper(root, 0)  
     def helper(self, curNode, pathSum):
-        if not curNode:  # If the current node is None, return 0
+        if not curNode:  
             return 0
 
-        # Update pathSum by shifting digits and adding the current node's value
         pathSum = 10 * pathSum + curNode.val
 
-        # If the current node is a leaf node (no left or right child), return pathSum
         if not curNode.left and not curNode.right:
             return pathSum
 
-        # Recursively calculate the sum of numbers from left and right subtrees
         return (
             self.helper(curNode.left, pathSum) +
             self.helper(curNode.right, pathSum)
