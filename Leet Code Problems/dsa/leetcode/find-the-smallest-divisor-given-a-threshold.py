50de34ff523f093b83d0fd4b4fcfc08691f93d41
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-03-14 18:25:40 +0300

    Add solution for Find the Smallest Divisor Given a Threshold

---
+++ Leet Code Problems/dsa/leetcode/find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def calculate_sum(divisor):
+            total_sum = 0
+            for num in nums:
+                total_sum += math.ceil(num / divisor)
+            return total_sum
+        
+        # Binary search 
+        def binary_search(low, high):
+            while low < high:
+                mid = (low + high) // 2
+                total_sum = calculate_sum(mid)
+                
+                if total_sum > threshold:
+                    low = mid + 1
+                else:
+                    high = mid
+            
+            return low
+        
+        # Finding the min divisor using bs within the range 1 to max(nums)
+        return binary_search(1, max(nums))
