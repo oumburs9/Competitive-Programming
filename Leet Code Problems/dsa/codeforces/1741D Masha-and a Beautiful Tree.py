b714762675fa09fef20346d647b79d01c4aa3f5d
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-04-10 20:49:38 +0300

    Add solution for Masha and a Beautiful Tree

---
+++ Leet Code Problems/dsa/codeforces/1741D Masha-and a Beautiful Tree.py
@@ -0,0 +1,29 @@
+def solve(l, r):
+    if r - l == 1:
+        return 0
+    mid = (l + r) // 2
+    mal = max(arr[l:mid])
+    mar = max(arr[mid:r])
+    
+    ans = 0
+    if mal > mar:
+        ans += 1
+        arr[l:mid], arr[mid:r] = arr[mid:r], arr[l:mid]
+    return solve(l, mid) + solve(mid, r) + ans
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        m = int(input())
+        global arr 
+        
+        arr = list(map(int, input().split()))
+        ans = solve(0, m)
+        
+        if sorted(arr) == arr:
+            print(ans)
+        else:
+            print(-1)
+
+if __name__ == "__main__":
+    main()
