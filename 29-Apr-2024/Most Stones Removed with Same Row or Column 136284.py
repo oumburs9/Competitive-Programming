6adf823f870446aee1863cab7f367453dfab5d0f
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-04-30 00:34:16 +0300

    Created 'Most Stones Removed with Same Row or Column' by A2SV Hub

---
+++ 29-Apr-2024/Most Stones Removed with Same Row or Column 136284.py
@@ -0,0 +1,43 @@
+# Problem: Most Stones Removed with Same Row or Column - https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        
+        uf = UnionFind()
+        
+        for x, y in stones:
+            uf.add(x)
+            uf.add(y + 10001)  
+            uf.union(x, y + 10001)
+        
+        unique_roots = len(set(uf.find(x) for x, _ in stones))
+        return len(stones) - unique_roots
+
+
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+    
+    def add(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+            self.rank[x] = 0
+
