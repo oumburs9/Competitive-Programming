033f0dbd26a36c079323f45deb9bcc7c1b8e0fdd
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-05-06 01:13:57 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 05-May-2024/Almost Prime 140874.py
@@ -0,0 +1,21 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def trial_division_simple(n: int):
+    factorization = set()
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factorization.add(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factorization.add(n)
+
+    return factorization
+cnt = 0
+n = int(input())
+for i in range(1,n+1):
+    res = trial_division_simple(i)
+    if len(res) == 2:
+        cnt += 1
+print(cnt)
