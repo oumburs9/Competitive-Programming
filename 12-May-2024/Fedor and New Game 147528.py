75db2dfe48259b711255c5eeb5a1f39afce9cf61
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-05-12 19:45:35 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 12-May-2024/Fedor and New Game 147528.py
@@ -0,0 +1,17 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+
+players = [int(input()) for _ in range(m + 1)]
+
+fe = players[-1]
+
+
+count = 0
+for p in players[:-1]:
+    if bin(p ^ fe).count('1') <= k:
+        count += 1
+
+
+
+print(count)
