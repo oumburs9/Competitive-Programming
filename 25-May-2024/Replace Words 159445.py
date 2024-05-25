08b72061cc29b45840abefe652b5395607a026ec
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-05-25 22:12:46 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 25-May-2024/Replace Words 159445.py
@@ -0,0 +1,41 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+    
+    def search_shortest_prefix(self, word):
+        node = self.root
+        prefix = ""
+        for char in word:
+            if char not in node.children:
+                break
+            node = node.children[char]
+            prefix += char
+            if node.is_end:
+                return prefix
+        return word
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for root in dictionary:
+            trie.insert(root)
+            
+        words = sentence.split()
+        replaced_words = [trie.search_shortest_prefix(word) for word in words]
+        return ' '.join(replaced_words)
+        
