f10f190565993d1d667e125423f03cf6b1a935b8
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-06-30 22:27:50 +0300

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 30-Jun-2024/Find the Town Judge 174995.py
@@ -0,0 +1,16 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusted = [0] * (n + 1)
+
+        for a, b in trust:
+            trusted[a] -= 1
+            trusted[b] += 1
+        
+        for i in range(1, n + 1):
+            if trusted[i] == n - 1:
+                return i
+        
+        return -1
+        
