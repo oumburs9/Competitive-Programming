92406a50877159411b9db30c0d390be84b00a5ff
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-01 22:12:53 +0300

    Created 'C - Kefa and Park' by A2SV Hub

---
+++ A2SV Remote Contest #21 01-Jul-2024/C - Kefa and Park 175347.py
@@ -0,0 +1,61 @@
+# Problem: C - Kefa and Park - https://codeforces.com/gym/532332/problem/C
+
+import sys
+from collections import defaultdict, deque
+
+input = sys.stdin.read
+_list = input().split()
+
+idx = 0
+n = int(_list[idx])
+m = int(_list[idx + 1])
+
+idx += 2
+
+cats = list(map(int, _list[idx:idx + n]))
+idx += n
+
+edges = []
+for _ in range(n - 1):
+    u = int(_list[idx])
+    v = int(_list[idx + 1])
+    edges.append((u, v))
+    idx += 2
+
+tree = defaultdict(list)
+for u, v in edges:
+    tree[u].append(v)
+    tree[v].append(u)
+
+visited = [False] * (n + 1)
+valid_rest = 0
+
+stack = [(1, 0, 0)]
+
+while stack:
+    node, conse_cats, parent = stack.pop()
+
+    if visited[node]:
+        continue
+
+    visited[node] = True
+
+    if cats[node - 1] == 1:
+        conse_cats += 1
+    else:
+        conse_cats = 0
+
+    if conse_cats > m:
+        continue
+
+    is_lf = True
+
+    for neighbor in tree[node]:
+        if not visited[neighbor] and neighbor != parent:
+            is_lf = False
+            stack.append((neighbor, conse_cats, node))
+
+    if is_lf:
+        valid_rest += 1
+
+print(valid_rest)
