bcee0a15e87bc81c74a98044b324d0a25fd716eb
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-01 22:13:47 +0300

    Created 'A - Forever Winter' by A2SV Hub

---
+++ A2SV Remote Contest #21 01-Jul-2024/A - Forever Winter 175351.py
@@ -0,0 +1,17 @@
+# Problem: A - Forever Winter - https://codeforces.com/gym/532332/problem/A
+
+inp = int(input())
+
+for i in range(inp):
+    n, m = map(int, input().split())
+    l = [0] * (n + 1)
+    
+    for j in range(m):
+        x, y = map(int, input().split())
+        l[x] += 1
+        l[y] += 1
+    
+    z = l.count(1)
+    X = n - z - 1
+    
+    print(X, z // X)
