6338044e46ece32c3cefb62b0a658c067ac52df8
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-01 22:13:32 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Remote Contest #21 01-Jul-2024/B - New Year Transportation 175348.py
@@ -0,0 +1,16 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/532332/problem/B
+
+n,m = map(int, input().split())
+_list = list(map(int, input().split()))
+
+cur = 1
+
+while cur < m:
+    cur += _list[cur - 1]
+
+
+if cur ==m:
+    print("YES")
+    
+else:
+    print("NO")
