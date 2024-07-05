3016b81a1fd84263fd4fd23a6a419ac8604f2e2a
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-05 18:02:15 +0300

    Created 'Selection Sort' by A2SV Hub

---
+++ 05-Jul-2024/Selection Sort 176622.py
@@ -0,0 +1,18 @@
+# Problem: Selection Sort - https://practice.geeksforgeeks.org/problems/selection-sort/1
+
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        min_idx = i
+        for j in range(i,len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        return min_idx
+            
+    
+    def selectionSort(self, arr,n):
+        for i in range(n):
+            min_idx = self.select(arr,i)
+            arr[i] , arr[min_idx] = arr[min_idx] ,arr[i]
+        # print(*arr)
