50357ad9fc433f7f28e8a77e99dd58bceece848b
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-07 21:58:16 +0300

    Created 'Honest Coach' by A2SV Hub

---
+++ 07-Jul-2024/Honest Coach 177229.py
@@ -0,0 +1,21 @@
+# Problem: Honest Coach - https://codeforces.com/problemset/problem/1360/B
+
+t = int(input()) 
+
+for _ in range(t):
+    n = int(input())
+    s = list(map(int, input().split()))  # Strengths of the athletes
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if s[j] > s[j+1]:
+                s[j], s[j+1] = s[j+1], s[j]
+
+    min_diff = 1001 
+
+    for i in range(1, n):
+        diff = s[i] - s[i-1]
+        if diff < min_diff:
+            min_diff = diff
+
+    print(min_diff)
