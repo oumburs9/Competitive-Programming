f67750a5106f92196b0150d9bbe477ecaf6ccd59
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-15 21:47:20 +0300

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/B - Guess the Maximum 180609.py
@@ -0,0 +1,11 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+test = int(input())
+for _ in range(test):
+    size = int(input())
+    nums = list(map(int, input().split()))
+    ans = max(nums)
+    for ind in range(size - 1):
+        maxNum = max(nums[ind], nums[ind + 1])
+        ans = min(ans, maxNum)
+    print(ans - 1)
