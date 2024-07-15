2a596878efd2b87678bc89e17191c64c7c478b05
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-15 21:47:09 +0300

    Created 'A - Binary Imbalance' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/A - Binary Imbalance 180608.py
@@ -0,0 +1,22 @@
+# Problem: A - Binary Imbalance - https://codeforces.com/gym/535309/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    zeros = s.count('0')
+    ones = s.count('1')
+    
+    if zeros > ones:
+        print('YES')
+    else:
+        flag = False
+        for j in range(n-1):
+            if s[j] != s[j+1]:
+                flag = True
+                break
+        if flag:
+            print("YES")
+        else:
+            print("NO")
