f1d5d008a195b9bca2ec9d5ebf6e6b267f6620b2
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-22 21:48:36 +0300

    Created 'A - Hit the Lottery' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/A - Hit the Lottery 182898.py
@@ -0,0 +1,14 @@
+# Problem: A - Hit the Lottery - https://codeforces.com/gym/536373/problem/A
+
+n = int(input())
+
+denoms = [100, 20, 10, 5,1]
+
+cnt = 0
+
+for denom in denoms:
+    cnt += n//denom
+    
+    n %= denom
+    
+print(cnt)
