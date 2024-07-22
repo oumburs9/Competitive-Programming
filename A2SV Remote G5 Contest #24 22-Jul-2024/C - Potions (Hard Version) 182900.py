c747b71afff993ad50ddc078f5a7984ecbf9e276
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-22 21:49:14 +0300

    Created 'C - Potions (Hard Version)' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/C - Potions (Hard Version) 182900.py
@@ -0,0 +1,25 @@
+# Problem: C - Potions (Hard Version) - https://codeforces.com/gym/536373/problem/C
+
+import heapq
+n = int(input())
+potions = list(map(int, input().split()))
+
+health = 0
+cnt  = 0
+min_heap = []
+
+for i in range(n):
+    if potions[i] >= 0:
+        health += potions[i]
+        cnt += 1
+    else:
+        if potions[i] + health >= 0:
+            health += potions[i]
+            cnt += 1
+            heapq.heappush(min_heap,potions[i])
+        elif min_heap and min_heap[0] < potions[i]:
+            health -= heapq.heappop(min_heap)
+            health += potions[i]
+            heapq.heappush(min_heap,potions[i])
+            
+print(cnt)
