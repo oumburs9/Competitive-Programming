ab2079389d5f221778e0d77e5431633fbbba0988
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-22 21:49:01 +0300

    Created 'B - Remove Smallest' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/B - Remove Smallest 182899.py
@@ -0,0 +1,22 @@
+# Problem: B - Remove Smallest - https://codeforces.com/gym/536373/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    positive_nums = list(map(int,input().split()))
+    
+    positive_nums.sort()
+    
+    flag = True
+    for i in range(n-1):
+        # if abs(positive_nums[i+1] - positive_nums[i]) <= 1:
+            # positive_nums.pop(0)
+            # print(positive_nums)
+        if abs(positive_nums[i+1] - positive_nums[i]) > 1:
+            flag = False
+            break
+        
+    if flag:
+        print("YES")
+    else:
+        print("NO")
