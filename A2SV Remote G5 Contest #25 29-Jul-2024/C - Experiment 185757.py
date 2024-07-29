ec84bb653c7bc93314b32e8b74ac515eb9ce6be1
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-29 21:39:32 +0300

    Created 'C - Experiment' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/C - Experiment 185757.py
@@ -0,0 +1,29 @@
+# Problem: C - Experiment - https://codeforces.com/gym/537575/problem/C
+
+n = int(input())
+experiments = []
+for _ in range(n):
+    si, ti, bi = map(int, input().split())
+    experiments.append((si, ti, bi))
+
+
+events = []
+for exp in experiments:
+    start, end, rooms = exp
+    
+    events.append((start, rooms))  
+    events.append((end + 1, -rooms))
+
+events.sort()
+
+max_rooms = 0
+current_rooms = 0
+
+for event in events:
+    time, room_change = event
+    current_rooms += room_change
+    if current_rooms > max_rooms:
+        max_rooms = current_rooms
+
+print(max_rooms)
+
