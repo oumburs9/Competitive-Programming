f6c994a86721f464178d378b4010572f47551125
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-07-29 21:39:06 +0300

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185756.py
@@ -0,0 +1,14 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+s = input()
+stack = []
+valid_count = 0
+
+for char in s:
+    if char == '(':
+        stack.append(char)
+    elif char == ')' and stack:
+        stack.pop()
+        valid_count += 2
+        
+print(valid_count)
