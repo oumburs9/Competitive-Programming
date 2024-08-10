6133fd87d2da5fc6f1e7f40d947adb40933c7bc5
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-10 20:59:28 +0300

    Created 'Map of Highest Peak' by A2SV Hub

---
+++ 10-Aug-2024/Map of Highest Peak 190345.py
@@ -0,0 +1,21 @@
+# Problem: Map of Highest Peak - https://leetcode.com/problems/map-of-highest-peak/description/
+
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        m,n = len(isWater),len(isWater[0])
+        q = deque()
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if isWater[i][j]==1:
+                    dp[i][j] = 0
+                    q.append([i,j,0])
+        
+        while q:
+            x, y, c = q.popleft()
+            for i,j in [(-1,0),(1,0),(0,1),(0,-1)]:
+                if 0 <= x+i < m and 0 <= y+j < n and dp[x+i][y+j] == float('inf'):
+                    dp[x+i][y+j] = c+1
+                    q.append([x+i,y+j,c+1])
+        return dp
+            
