e3062fe11df40b4e99439d61db6adbf9dbf4c082
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-16 21:31:32 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 16-Aug-2024/New Year Transportation 192144.py
@@ -0,0 +1,21 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+def can_reach(n, t, a):
+    current_position = 1
+    while current_position < t:
+        current_position += a[current_position - 1]
+    return current_position == t
+
+
+
+#input
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+
+
+# chec if can reach t
+if can_reach(n, t, a):
+    print("YES")
+else:
+    print("NO")
