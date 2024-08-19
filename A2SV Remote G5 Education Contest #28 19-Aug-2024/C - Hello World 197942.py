88d43120a97d9bb0e4fc44d0310e3e50552130bd
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-19 20:59:29 +0300

    Created 'C - Hello World' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 19-Aug-2024/C - Hello World 197942.py
@@ -0,0 +1,28 @@
+# Problem: C - Hello World - https://codeforces.com/gym/543262/problem/C
+
+t = int(input())
+res = []
+
+for _ in range(t):
+    n = int(input())
+    c = list(map(int, input().split())  )
+    c.sort()
+
+    if c[0] != 1:
+        res.append("NO")
+    else:
+        s = 1  # Start with a sum of 1
+        ok = True
+        for i in range(1, n):
+            if c[i] > s:
+                ok = False
+                break
+            s += c[i]
+        
+        if ok:
+            res.append("YES" )
+        else:
+            res.append("NO" )
+
+for r in res:
+    print(r)
