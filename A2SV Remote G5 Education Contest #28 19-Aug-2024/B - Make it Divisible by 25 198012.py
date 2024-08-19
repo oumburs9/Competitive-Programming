bb4738cf0b9039419d940d2a5d8918e281823749
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-19 22:56:50 +0300

    Created 'B - Make it Divisible by 25' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 19-Aug-2024/B - Make it Divisible by 25 198012.py
@@ -0,0 +1,32 @@
+# Problem: B - Make it Divisible by 25 - https://codeforces.com/gym/543262/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = input()
+
+    length = len(n)
+
+    min_steps = float('inf')
+    
+ 
+    targets = ['00', '25', '50', '75']
+    
+    for target in targets:
+        pos = len(target) - 1
+        count = 0
+        for i in range(length - 1, -1, -1):
+
+            if n[i] == target[pos]:
+                pos -= 1
+
+                if pos < 0:
+                    break
+            else:
+                count += 1
+        
+        if pos < 0:
+
+            min_steps = min(min_steps, count)
+
+    
+    print(min_steps)
