ecac5d16778d8767494b26b7d3c3c452653cca02
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-22 21:54:14 +0300

    Created 'E - Office Keys' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 22-Aug-2024/E - Office Keys 198718.py
@@ -0,0 +1,28 @@
+# Problem: E - Office Keys - https://codeforces.com/gym/543262/problem/E
+
+n, k, room = list(map(int, input().split()))
+people = sorted(list(map(int, input().split())))
+keys = sorted(list(map(int, input().split())))
+
+def can_reach(time):
+    j = 0
+    for i in range(n):
+        
+        while j < k and abs(keys[j] - people[i]) + abs(keys[j] - room) > time:
+            j += 1
+        if j == k:
+            return False
+        j += 1 
+    return True
+
+l = 0
+r = int(1e18)
+
+while l < r:
+    mid = (l + r) // 2
+    if can_reach(mid):
+        r = mid
+    else:
+        l = mid + 1
+
+print(l)
