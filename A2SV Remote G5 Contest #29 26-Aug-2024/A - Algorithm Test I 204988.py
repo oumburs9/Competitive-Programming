16641ec1600bb4d92df759098020b4588eee20d2
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-26 23:44:10 +0300

    Created 'A - Algorithm Test I' by A2SV Hub

---
+++ A2SV Remote G5 Contest #29 26-Aug-2024/A - Algorithm Test I 204988.py
@@ -0,0 +1,11 @@
+# Problem: A - Algorithm Test I - https://codeforces.com/gym/544347/problem/A
+
+import math
+
+n = int(input())
+colors = list(map(int, input().split()))
+unique_cnt = len(set(colors))
+
+res = math.factorial(unique_cnt)
+
+print(res)
