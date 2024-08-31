8bc5dc2a8bf399bf178ad5c776a6940b0b1c0a42
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-08-31 23:09:47 +0300

    Created 'B - Amro and Bits' by A2SV Hub

---
+++ A2SV Remote Contest #30 31-Aug-2024/B - Amro and Bits 209994.py
@@ -0,0 +1,28 @@
+# Problem: B - Amro and Bits - https://codeforces.com/gym/545837/problem/B
+
+
+t = int(input())
+
+res = []
+
+for _ in range(t):
+    x = int(input())
+    
+    if x == 1:
+        res.append(3)
+        
+    elif x % 2 == 1:
+        res.append(1)
+        
+    elif (x & (x - 1)) == 0:
+        res.append(x + 1)
+        
+    else:
+        y = 1
+        while x & y == 0:
+            y <<=1
+            
+        res.append(y)
+
+for r in res:
+    print(r)
