6b363c3dd22c30f8740ed1e099f0378b06022513
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-09-02 22:56:11 +0300

    Created 'E - Good Will Hunting' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/E - Good Will Hunting 210536.py
@@ -0,0 +1,17 @@
+# Problem: E - Good Will Hunting - https://codeforces.com/gym/545837/problem/E
+
+l, r = map(int, input().split())
+
+a = l ^ r
+
+nums = list(bin(a)[2:])
+
+if '1' not in nums:
+    print(0)
+else:
+    idx = nums.index('1')
+    for i in range(idx, len(nums)):
+        nums[i] = '1'
+        
+    s = ''.join(nums)
+    print(int(s, 2))
