c9d1ccb206492e9c060b2f30284a71cd0161a4c2
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-09-02 21:53:20 +0300

    Created 'D - Firaol With His Game' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/D - Firaol With His Game 210484.py
@@ -0,0 +1,29 @@
+# Problem: D - Firaol With His Game - https://codeforces.com/gym/545837/problem/D
+
+import heapq
+
+
+t = int(input())
+
+for _ in range(t):
+    challanges, time = map(int, input().split())
+    times = list(map(int, input().split()))
+    total = sum(times)
+
+    if time >= total:
+        print(0)
+        continue
+    curr = 0
+    heap = []
+    ans = 0
+
+    for ind in range(challanges):
+        t = times[ind]
+        curr += t
+        heapq.heappush(heap, (-t, ind))
+        if curr > time:
+            timeTaken, ind = heapq.heappop(heap)
+            ans = ind + 1
+            break
+
+    print(ans)
