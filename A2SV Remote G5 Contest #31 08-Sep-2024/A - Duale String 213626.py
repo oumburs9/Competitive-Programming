6ed8a299006c404630298223a29b2d820e8283d0
Jabir Esmael Oumer <91590617+oumburs9@users.noreply.github.com>
2024-09-08 23:16:04 +0300

    Created 'A - Duale String' by A2SV Hub

---
+++ A2SV Remote G5 Contest #31 08-Sep-2024/A - Duale String 213626.py
@@ -0,0 +1,17 @@
+# Problem: A - Duale String - https://codeforces.com/gym/547609/problem/A
+
+test  = int(input())
+
+for _ in range(test):
+    _str = input().strip()
+    
+    n =  len(_str)
+    
+    # if odd
+    if n% 2 == 1:
+        print('NO')
+    else:
+        if _str[:n//2] == _str[n//2:]:
+            print('YES')
+        else:
+            print('NO')
